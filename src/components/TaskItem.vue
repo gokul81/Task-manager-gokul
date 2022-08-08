c5da213005e91cb7d49c2087125dfb9ee65f6d20
gokul81 <105714204+gokul81@users.noreply.github.com>
2022-08-08 16:35:05 -0700

    input field changes

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -9,7 +9,13 @@ export default {
   <div
     class="p-1 rounded-lg mb-1 bg-teal-600 flex justify-between border-slate-900 shadow-2xl mt-4 text-xl"
   >
-    {{ item.title }}
+    <div class="flex gap-5">
+      <input class="check text-black line-through" type="checkbox" />
+      <label>
+        {{ item.title }}
+      </label>
+    </div>
+
     <div class="flex row-reverse" style="gap: 1rem">
       <div @click="this.$emit('onShow', item.id)" class="text-blue-700 text-xl">
         <slot></slot>
@@ -17,3 +23,16 @@ export default {
     </div>
   </div>
 </template>
+
+<style scoped>
+input[type="checkbox"]:checked + label {
+  color: black;
+  text-decoration: line-through;
+}
+input.check {
+  width: 20px;
+  height: 20px;
+  margin: auto;
+  cursor: pointer;
+}
+</style>
